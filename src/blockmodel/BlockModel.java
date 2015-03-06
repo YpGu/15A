f6215a85525e306623b1f0132cd2ab673358c086
YpGu <gyp7364@gmail.com>
2015-03-06 14:36:48 -0500

    bug fix

--- src/blockmodel/BlockModel.java
+++ src/blockmodel/BlockModel.java
@@ -12,21 +12,24 @@ import java.util.*;
 
 public class BlockModel
 {
-	public final static int NUM_BLOCKS = 10;
-	public final static int MAX_ITER = 100;
-	public final static int NUM_INITS = 5;							// init the configuration multiple times, and keep the one with largest likelihood 
+	public static int NUM_BLOCKS;							// Number of Blocks (pre-defined) 
+	public final static int MAX_ITER = 100;						// Maximum number of iterations until convergence 
+	public final static int NUM_INITS = 5;						// init the configuration multiple times, and keep the one with largest likelihood 
+	public final static boolean WRITE = false;
 	public static String fileDir, dictDir;
 
-	public static Map<String, Integer> idMap;						// RawID -> newID 
-	public static Map<String, Integer> z;							// Block Assignment
-	public static double[][] eta;								// Block Parameters (K*K) 
-
-	public static SparseMatrix trainData, testData;
-
-	public static Map<String, Integer> optZ;
-	public static double[][] optEta;
+	public static SparseMatrix trainData, testData;					// Graph data (training/testing) 
+	public static Map<String, Integer> z;						// Block Assignment
+	public static Map<String, Integer> optZ;					// [Optimal] Block Assignment 
+	public static double[][] eta;							// Block Parameters (K*K) 
+	public static double[][] optEta;						// [Optimal] Block Parameters 
 
 	public static Scanner sc;
+
+
+	public BlockModel() {
+		optZ = new HashMap<String, Integer>();
+	}
 
 	/// initialize parameters 
 	public static void
@@ -34,6 +37,7 @@ public class BlockModel
 
 		fileDir = args[0];
 		dictDir = args[1];
+		NUM_BLOCKS = Integer.parseInt(args[2]);
 
 		eta = new double[NUM_BLOCKS][NUM_BLOCKS];
 
@@ -43,7 +47,6 @@ public class BlockModel
 		FileParser.readData(dictDir, fileDir + ".test", testData);
 
 		z = new HashMap<String, Integer>();
-		optZ = new HashMap<String, Integer>();
 
 		// random initialization - z 
 		Random rand = new Random(10*init+1);
@@ -62,8 +65,8 @@ public class BlockModel
 			m[zx][zy] += v;
 		}
 		double[] counter = new double[NUM_BLOCKS];				// counter (K*1): #Block -> num of nodes 
-		for (String i: trainData.getDict()) {					// TODO: if we iterate through trainData.dict, some nodes will never appear in the actual training dataset (in test only) 
-			int block = z.get(i);
+		for (String s: trainData.getDict()) {					// TODO: if we iterate through trainData.dict, some nodes will never appear in the actual training dataset (in test only) 
+			int block = z.get(s);
 			counter[block] += 1;
 		}
 		for (int i = 0; i < NUM_BLOCKS; i++) {
@@ -137,7 +140,7 @@ public class BlockModel
 			}
 
 //// check blocks
-			double[] counter = new double[NUM_BLOCKS];				// counter (K*1): #Block -> num of nodes 
+			double[] counter = new double[NUM_BLOCKS];			// counter (K*1): #Block -> num of nodes 
 			for (Map.Entry<String, Integer> i: z.entrySet()) {
 				int block = i.getValue();
 				counter[block] += 1;
@@ -167,7 +170,7 @@ public class BlockModel
 
 			for (int i = 0; i < NUM_BLOCKS; i++) {
 				for (int j = 0; j < NUM_BLOCKS; j++) {
-					double pb = counter[i] * counter[j];			// might be too large for almost all the block pairs 
+					double pb = counter[i] * counter[j];		// might be too large for almost all the block pairs 
 					if (i == j) {
 						pb -= counter[i];
 					}
@@ -204,63 +207,66 @@ public class BlockModel
 	/// main entry
 	public static void
 	main(String[] args) {
-		if (args.length != 2) {
-			System.out.println("Usage: java BlockModel <fileDir> <dictDir>");
-			System.out.println("Example: java BlockModel ../../data/3k_mention/mention_list_3k ../../data/3k_mention/mention_dict_3k");
+		if (args.length != 3) {
+			System.out.println("Usage: java BlockModel <fileDir> <dictDir> <#blocks>");
+			System.out.println("Example: java BlockModel ../../data/3k_mention/mention_list_3k ../../data/3k_mention/mention_dict_3k 10");
 			System.exit(0);
 		}
 
 		double maxObj = -Double.MAX_VALUE, curObj;
-		int init = 0;
-		double[] counter = new double[NUM_BLOCKS];				// counter (K*1): #Block -> num of nodes 
-		boolean zeroFlag = true;
+		int init = 1;
 		while (true) {
-			if (init > NUM_INITS) {
-				break;
-			}
-
-			System.out.println("------ Initialization #" + init);
+			System.out.println("------ Initialization #" + init + " ------");
 			init(args, init);
 			curObj = train(trainData);
 			System.out.println("Objective function = " + curObj);
 			if (curObj > maxObj) {
 				maxObj = curObj;
 				optEta = eta;
-				optZ = z;
+				System.out.println("optZ updated!");
+				optZ = new HashMap<String, Integer>(z);
 			}
-			init += 1;
 
 			// check empty blocks (for [current] block): break if all blocks are non-empty 
+			double[] tmpCounter = new double[NUM_BLOCKS];
 			for (int k = 0; k < NUM_BLOCKS; k++) {
-				counter[k] = 0;
+				tmpCounter[k] = 0;
 			}
 			for (Map.Entry<String, Integer> i: z.entrySet()) {
 				int block = i.getValue();
-				counter[block] += 1;
+				tmpCounter[block] += 1;
 			}
+			double zeroFlag = 1;
 			for (int k = 0; k < NUM_BLOCKS; k++) {
-				zeroFlag = zeroFlag && (counter[k] != 0);
+				zeroFlag *= tmpCounter[k];
 			}
-			if (zeroFlag) {
-				break;
+			if (zeroFlag != 0) {
+				if (init >= NUM_INITS) {
+					break;			// at most NUM_INITS number of initializations 
+				}
 			}
+
+			init += 1;
 		}
 
 		// output z
+		int[] counter = new int[NUM_BLOCKS];
 		for (int k = 0; k < NUM_BLOCKS; k++) {
 			counter[k] = 0;
 		}
-		for (Map.Entry<String, Integer> i: z.entrySet()) {
-			int block = i.getValue();
+		for (Map.Entry<String, Integer> e: optZ.entrySet()) {
+			int block = e.getValue();
 			counter[block] += 1;
 		}
-		for (int i = 0; i < counter.length; i++) {
-			System.out.printf("\t%d", (int)counter[i]);
+		for (int i = 0; i < NUM_BLOCKS; i++) {
+			System.out.printf("\t%d", counter[i]);
 		}
 		System.out.printf("\n");
 
-//		FileParser.output("./res/z_"+NUM_BLOCKS, optZ);
-//		FileParser.output("./res/eta_"+NUM_BLOCKS, optEta);
+		if (WRITE) {
+			FileParser.output("./res/z_"+NUM_BLOCKS, optZ);
+			FileParser.output("./res/eta_"+NUM_BLOCKS, optEta);
+		}
 
 		return;
 	}
