235f8555fff8547b03e562c130c1936a51cbafc2
YpGu <gyp7364@gmail.com>
2015-03-30 17:23:08 -0400

    init multinomial mixtures

---
+++ src/multinomial/Mixture.java
@@ -0,0 +1,14 @@
+/**
+	03/30/2015 
+
+	Mixture.java: implement the unified model with two multinomial mixtures
+
+	The first mixture is background
+	The second mixture is ideal point
+
+
+**/
+
+public class Mixture
+{
+}
